1201037f4c0a9c31d27675713f428e65b0f1ddab
makami45 <longnhatyi@gmail.com>
2023-05-02 20:28:51 +0700

    yu

--- caidat.cpp
+++ caidat.cpp
@@ -2,7 +2,15 @@
 
 void nhap_songuyen(int& n) {
 	do {
-		cout << "Nhap so nguye n (n>0): ";
+		cout << "Nhap so nguyen n (n>0): ";
 		cin >> n;
 	} while (n <= 0);
+}
+bool kt_snt(int n) {
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		if (n % i == 0) count++;
+		if (count == 2) return true;
+		return false;
+	}
 }
